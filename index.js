eece6e71cd5b5fda9b276ed60c7384877bfd80d1
Naveed Shah <naveed@tayybah.com>
2016-12-15 12:39:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking(){
+    return setInterval(() => this.time++, 1000);
+  }
+}
+
+function censor(word, string){
+  return string.replace(new RegExp(word, 'g'), 'BLEEP');
+}
+
+const violenceCensor = censor.bind(null,'violence');
+
+const drugsCensor = censor.bind(null,'drugs');
